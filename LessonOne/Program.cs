3657f1c6948ec2c52870d62b020c51d49fab63fd
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-07-02 23:04:50 +0300

    saveMetod

--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -3,4 +3,17 @@
 string[] arrayStart = new string[5] {"126", "2e4r", "nod", "1", "sta"}; 
 string[] arrayEnd = new string[arrayStart.Length];
 
+void InputArray(string[] arrayStart, string[] arrayEnd)
+{
+    int count = 0;
+    {
+    for (int i = 0; i < arrayStart.Length; i++)
+        if(arrayStart[i].Length <= 3)
+        {
+            arrayEnd[count] = arrayStart[i];
+            count++;
+        }
+    }
+}
+
 
